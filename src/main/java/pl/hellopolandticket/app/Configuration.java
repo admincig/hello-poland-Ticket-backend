8f51ab2fe6374240fc318a2bf9d6ecbb082b6c0c
Konrad Bączyński <konrad.baczynski@empirica.pl>
2018-03-20 14:54:55 +0100

    changed to jdk8

--- src/main/java/pl/hellopolandticket/app/Configuration.java
+++ src/main/java/pl/hellopolandticket/app/Configuration.java
@@ -11,14 +11,11 @@ import javax.enterprise.inject.Produces;
 @Startup
 public class Configuration {
 
-  private System.Logger logger = System.getLogger(Configuration.class.getName());
-
   public Configuration() {
     java.util.Properties systemProps = System.getProperties();
     try (InputStream customProps = Configuration.class.getResourceAsStream("/config.properties")) {
       systemProps.load(customProps);
     } catch (IOException e) {
-      logger.log(System.Logger.Level.WARNING, "Failed to load custom properties", e);
     }
   }
 
