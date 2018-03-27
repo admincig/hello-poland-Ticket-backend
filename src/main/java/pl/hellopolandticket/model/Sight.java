cdc336224719fde516bebd2b675faa9a41acb2f3
Konrad Bączyński <konrad.baczynski@empirica.pl>
2018-03-27 14:50:47 +0200

    removed useless imports

--- src/main/java/pl/hellopolandticket/model/Sight.java
+++ src/main/java/pl/hellopolandticket/model/Sight.java
@@ -1,18 +1,13 @@
 package pl.hellopolandticket.model;
 
-import static javax.persistence.FetchType.EAGER;
-
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import lombok.Builder;
