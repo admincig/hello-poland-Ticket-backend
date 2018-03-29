56401182542bf4c02e513cad5b797692c23bd453
Konrad Bączyński <konrad.baczynski@empirica.pl>
2018-03-29 16:43:16 +0200

    configured tests

--- src/test/java/pl/hellopolandticket/service/BookingCancelSchedulerTest.java
+++ src/test/java/pl/hellopolandticket/service/BookingCancelSchedulerTest.java
@@ -1,8 +1,10 @@
 package pl.hellopolandticket.service;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -12,9 +14,6 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import pl.hellopolandticket.dao.SightDao;
-import pl.hellopolandticket.dao.TicketDao;
-import pl.hellopolandticket.dao.TicketDefinitionDao;
 import pl.hellopolandticket.model.Sight;
 
 @Slf4j
@@ -24,19 +23,23 @@ public class BookingCancelSchedulerTest {
   @Inject
   private SightService sightService;
 
+  @PersistenceContext(unitName = "test")
+  EntityManager entityManager;
+
   @Deployment
   public static Archive<WebArchive> createDeployment() {
     return ShrinkWrap
         .create(WebArchive.class, "test.war")
-        .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
-        .addClasses(SightService.class, SightDao.class, TicketDao.class, TicketDefinitionDao.class);
+        .addAsResource("META-INF/persistence.xml")
+        .addPackages(true, "pl.hellopolandticket");
   }
 
   @Test
-  @UsingDataSet("datasets/sights.yml")
-  public void testCars() {
-    Sight s = sightService.findBySightName("Kolejkowo");
-    assertTrue(true);
+  @UsingDataSet("datasets/import.yml")
+  public void firstTest() {
+    String kolejkowo = "Kolejkowo";
+    Sight s = sightService.findBySightName(kolejkowo);
+    assertEquals(kolejkowo, s.getName());
 
   }
 }
