01f5091ed2599c11c65ef4f088da67dce0596508
Konrad Bączyński <konrad.baczynski@empirica.pl>
2018-04-04 14:23:10 +0200

    fixed import.sql

--- src/main/resources/META-INF/import.sql
+++ src/main/resources/META-INF/import.sql
@@ -1,7 +1,4 @@
-INSERT INTO application_properties ("application_property_id", "property_name", "property_value")
-VALUES (1, 'rest.url.sightsUpload', 'https://hpl.fream.pl/api/v1/anything')
-INSERT INTO application_properties ("application_property_id", "property_name", "property_value")
-VALUES (2, 'rest.url.ticketsUpload', 'https://hpl.fream.pl/api/v1/anything')
-INSERT INTO application_properties ("application_property_id", "property_name", "property_value")
-VALUES (3, 'ticket.booked.timeToBuy', '20')
+INSERT INTO application_properties ("application_property_id", "property_name", "property_value") VALUES (1, 'rest.url.sightsUpload', 'https://hpl.fream.pl/api/v1/anything')
+INSERT INTO application_properties ("application_property_id", "property_name", "property_value") VALUES (2, 'rest.url.ticketsUpload', 'https://hpl.fream.pl/api/v1/anything')
+INSERT INTO application_properties ("application_property_id", "property_name", "property_value") VALUES (3, 'ticket.booked.timeToBuy', '20')
 
