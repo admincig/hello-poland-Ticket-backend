7b3b3c557b4b3d532c13afcb4b088fad02ac0a7f
Konrad Bączyński <konrad.baczynski@empirica.pl>
2018-04-06 15:18:41 +0200

    added generating a qr code

--- src/main/java/pl/hellopolandticket/rest/TicketRestService.java
+++ src/main/java/pl/hellopolandticket/rest/TicketRestService.java
@@ -4,8 +4,10 @@ import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.validation.Valid;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -28,4 +30,11 @@ public class TicketRestService {
 
     return Response.ok(ticketDTOs).build();
   }
+
+  @GET
+  @Path("/qr-code/encode/{ticketId}")
+  public Response encodeSerialNumber(@PathParam("ticketId") Long ticketId) {
+    return Response.ok(ticketService.encodeSerialNumberAsQrCode(ticketId)).build();
+  }
+
 }
