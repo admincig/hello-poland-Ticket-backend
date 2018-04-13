8fc400e5d50879bf81f4643d994aa689fda1cba9
Konrad Bączyński <konrad.baczynski@empirica.pl>
2018-04-13 11:47:43 +0200

    changed booking and buying tickets for presentation

--- src/main/java/pl/hellopolandticket/rest/BookingRestService.java
+++ src/main/java/pl/hellopolandticket/rest/BookingRestService.java
@@ -1,7 +1,10 @@
 package pl.hellopolandticket.rest;
 
+import static java.util.Arrays.asList;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -12,6 +15,7 @@ import javax.ws.rs.core.Response;
 import pl.hellopolandticket.service.BookingService;
 import pl.hellopolandticket.service.dto.BookingDTO;
 import pl.hellopolandticket.service.dto.BookingDTOCreate;
+import pl.hellopolandticket.service.dto.TicketBookingDTO;
 
 @Path("/bookings")
 @RequestScoped
@@ -32,9 +36,24 @@ public class BookingRestService {
     return Response.ok(bookingService.markBookingAsBought(bookingId)).build();
   }
 
-  @PUT
-  @Path("/book-buy")
-  public Response bookAndBuy(BookingDTOCreate booking) {
+  @GET
+  @Path("/book-buy/{email}")
+  public Response bookAndBuy(@PathParam("email") String email) {
+    TicketBookingDTO ticketBooking1 = TicketBookingDTO.builder()
+        .ticketDefinitionId(1L)
+        .numberOfTickets(2L)
+        .build();
+    TicketBookingDTO ticketBooking2 = TicketBookingDTO.builder()
+        .ticketDefinitionId(2L)
+        .numberOfTickets(3L)
+        .build();
+
+    BookingDTOCreate booking = BookingDTOCreate.builder()
+        .customerName("Jan Kowalski")
+        .customerEmail(email)
+        .ticketBookings(asList(ticketBooking1, ticketBooking2))
+        .build();
+
     BookingDTO persistedBooking = bookingService.createBooking(booking);
 
     return Response.ok(bookingService.markBookingAsBought(persistedBooking.getId())).build();
