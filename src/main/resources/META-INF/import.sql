d5df38fc838de81eb27680d2ab31f3fb4cf5c349
Konrad Bączyński <konrad.baczynski@empirica.pl>
2018-06-12 08:57:42 +0200

    changed request address

--- src/main/resources/META-INF/import.sql
+++ src/main/resources/META-INF/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO application_properties ("application_property_id", "property_name", "property_value") VALUES (1, 'rest.url.sightEventsUpload', 'https://hpl.fream.pl/api/v1/sights')
+INSERT INTO application_properties ("application_property_id", "property_name", "property_value") VALUES (1, 'rest.url.sightEventsUpload', 'https://api.hello-poland.pl/v1/sights')
 INSERT INTO application_properties ("application_property_id", "property_name", "property_value") VALUES (2, 'rest.url.ticketsUpload', 'https://api.hello-poland.pl/v1/sights')
 INSERT INTO application_properties ("application_property_id", "property_name", "property_value") VALUES (3, 'ticket.booked.timeToBuy', '20')
 INSERT INTO application_properties ("application_property_id", "property_name", "property_value") VALUES (4, 'ticket.qrCode.height', '200')
