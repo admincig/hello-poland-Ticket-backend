dd2b3739b950076b5d52cfc4af15ad34f840169e
Konrad Bączyński <konrad.baczynski@empirica.pl>
2018-07-17 09:14:34 +0200

    added example ticket pools

--- src/main/java/pl/hellopolandticket/model/ticket/partner/TicketDefinition.java
+++ src/main/java/pl/hellopolandticket/model/ticket/partner/TicketDefinition.java
@@ -66,7 +66,7 @@ public class TicketDefinition implements Serializable {
 
   @Setter
   @ManyToOne
-  @JoinColumn(name = "ticketPool")
+  @JoinColumn(name = "TICKET_POOL_ID")
   private TicketPool ticketPool;
 
   @Setter
