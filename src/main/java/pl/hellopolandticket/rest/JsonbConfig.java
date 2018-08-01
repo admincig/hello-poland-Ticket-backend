44ccb9c6d09797e78026b337392cd068c51d2bd0
Dawid Olejnik <dawid@fream.pl>
2018-08-01 15:10:38 +0200

    date formatter

--- src/main/java/pl/hellopolandticket/rest/JsonbConfig.java
+++ src/main/java/pl/hellopolandticket/rest/JsonbConfig.java
@@ -8,11 +8,20 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class JsonbConfig implements ContextResolver<Jsonb> {
 
+  private static Jsonb instance;
+
+  public static Jsonb getInstance() {
+    if (instance == null) {
+      javax.json.bind.JsonbConfig config =
+          new javax.json.bind.JsonbConfig().withDateFormat("yyyy-MM-dd'T'HH:mm:ssxxx", null);
+      instance = JsonbBuilder.create(config);
+    }
+    return instance;
+  }
+
   @Override
   public Jsonb getContext(Class<?> type) {
-    javax.json.bind.JsonbConfig config =
-        new javax.json.bind.JsonbConfig().withDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", null);
-    return JsonbBuilder.create(config);
+    return getInstance();
   }
 
 }
