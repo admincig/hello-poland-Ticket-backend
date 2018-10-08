213673538f9c5430e9eae619393a86f15e05366a
lmrozek <lmrozek@fream.pl>
2018-10-08 12:44:22 +0200

    hpl-421

--- src/main/java/pl/hellopolandticket/service/TicketService.java
+++ src/main/java/pl/hellopolandticket/service/TicketService.java
@@ -2,6 +2,8 @@ package pl.hellopolandticket.service;
 
 import static pl.hellopolandticket.model.ticket.market.Status.PUNCHED;
 import static pl.hellopolandticket.service.util.ModelObjectsToDTOConverter.ofTicket;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
 import java.util.Date;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
@@ -17,6 +19,7 @@ import pl.hellopolandticket.service.validator.TicketValidator;
 @Stateless
 @LocalBean
 public class TicketService extends ServiceSuperclass {
+  private static final Logger HELPDESK_LOGGER = System.getLogger("helpdesk-TicketService");
 
   @Inject
   private TicketDao ticketDao;
@@ -26,6 +29,9 @@ public class TicketService extends ServiceSuperclass {
 
   @Inject
   private UserService userService;
+
+  @Inject
+  private SightEventService sightEventService;
 
   public TicketDTO punchTicket(CurrentUser currentUser, Long sightEventId, String serialNumber) {
     Ticket ticket = ticketDao.findBySerialNumber(serialNumber);
@@ -45,6 +51,11 @@ public class TicketService extends ServiceSuperclass {
     ticket.setTicketTaker(ticketTaker);
     ticket.setStatus(PUNCHED);
     ticket.setPunchingDate(new Date());
+
+    HELPDESK_LOGGER.log(Level.INFO,
+        "BILETER: " + ticketTaker.getName() + " | PARTNER: " + ticketTaker.getPartner()
+            + " | KLIENT: " + ticket.getBooking().getCustomerName() + " | WYDARZENIE: "
+            + sightEventService.findSightEventById(sightEventId).getName());
 
     return ofTicket(ticket);
   }
