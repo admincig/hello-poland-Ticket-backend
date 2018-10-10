537854da7abbc391ba9ba04ab443b9d6f48ed5d2
lmrozek <lmrozek@fream.pl>
2018-10-10 19:06:59 +0200

    refactor

--- src/main/java/pl/hellopolandticket/rest/HPHelpdeskRestService.java
+++ src/main/java/pl/hellopolandticket/rest/HPHelpdeskRestService.java
@@ -19,41 +19,30 @@ public class HPHelpdeskRestService extends RestServiceSuperclass {
     String serverLogDirProp = System.getProperty("jboss.server.log.dir");
     serverLogDir = serverLogDirProp.endsWith("/") ? (serverLogDirProp) : (serverLogDirProp + "/");
   }
-  private static final String ORDERS_LOG_DIR = serverLogDir + "helpdesk/orders/";
-  private static final String TICKETS_LOG_DIR = serverLogDir + "helpdesk/tickets/";
+  private static final String ORDERS_LOG_PATH =
+      serverLogDir + "helpdesk/orders/helpdesk-orders.log";
+  private static final String TICKETS_LOG_PATH =
+      serverLogDir + "helpdesk/tickets/helpdesk-tickets.log";
 
   @GET
   @Path("/orders/{date}")
   public Response getOrdersLogs(@PathParam("date") String date) {
-    String logPathStr = "";
-    String now = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(LocalDate.now());
-    if (now.equals(date)) {
-      logPathStr = ORDERS_LOG_DIR + "helpdesk-orders.log";
-    } else {
-      logPathStr = ORDERS_LOG_DIR + "helpdesk-orders.log." + date;
-    }
-    java.nio.file.Path logPath = Paths.get(logPathStr);
-    if (Files.notExists(logPath)) {
-      return Response.ok("Nie znaleziono logów dla podanej daty.").build();
-    }
-    try {
-      var sb = new StringBuilder();
-      Files.readAllLines(logPath).forEach(line -> sb.append(line).append(System.lineSeparator()));
-      return Response.ok(sb.toString().length() == 0 ? "Brak wpisów." : sb.toString()).build();
-    } catch (IOException e) {
-      return Response.status(500, e.getMessage()).build();
-    }
+    return getLogResponse(date, ORDERS_LOG_PATH);
   }
 
   @GET
   @Path("/tickets/{date}")
   public Response getticketsLogs(@PathParam("date") String date) {
+    return getLogResponse(date, TICKETS_LOG_PATH);
+  }
+
+  private Response getLogResponse(String date, String path) {
     String logPathStr = "";
     String now = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(LocalDate.now());
     if (now.equals(date)) {
-      logPathStr = TICKETS_LOG_DIR + "helpdesk-tickets.log";
+      logPathStr = path;
     } else {
-      logPathStr = TICKETS_LOG_DIR + "helpdesk-tickets.log." + date;
+      logPathStr = path + "." + date;
     }
     java.nio.file.Path logPath = Paths.get(logPathStr);
     if (Files.notExists(logPath)) {
