5f28d6088302b13a1aadce0353e32fd3924a90ce
lmrozek <lmrozek@fream.pl>
2018-10-22 13:48:14 +0200

    update SightEvent can change flags blocked and published into false - fixed

--- src/main/java/pl/hellopolandticket/service/SightEventService.java
+++ src/main/java/pl/hellopolandticket/service/SightEventService.java
@@ -146,14 +146,16 @@ public class SightEventService extends ServiceSuperclass {
 
   public SightEventDTO updateSightEvent(Long sightId, SightEventDTO sightEventDTO) {
     SightEvent sightEvent = sightEventDao.findById(sightId);
-
     sightEvent.setName(sightEventDTO.name);
     sightEvent.setDescription(sightEventDTO.description);
     sightEvent.setEmail(sightEventDTO.email);
     sightEvent.setPhone(sightEventDTO.phone);
-    sightEvent.setBlocked(sightEventDTO.blocked);
-    sightEvent.setPublished(sightEventDTO.published);
-
+    if (sightEventDTO.blocked != null) {
+      sightEvent.setBlocked(sightEventDTO.blocked);
+    }
+    if (sightEventDTO.published != null) {
+      sightEvent.setPublished(sightEventDTO.published);
+    }
     String mainImageUrl = null;
     if (sightEventDTO.mainImage != null) {
       mainImageUrl = sightEventDTO.mainImage.original;
