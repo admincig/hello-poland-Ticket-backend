00c3f8a53304a54f4862e39457202b0d2fccce12
lmrozek <lmrozek@fream.pl>
2018-12-10 11:27:46 +0100

    endpoint ping

--- src/main/java/pl/hellopolandticket/rest/RestServiceSuperclass.java
+++ src/main/java/pl/hellopolandticket/rest/RestServiceSuperclass.java
@@ -1,12 +1,21 @@
 package pl.hellopolandticket.rest;
 
 import javax.interceptor.Interceptors;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import pl.hellopolandticket.app.LoggingHandler;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Interceptors(value = LoggingHandler.class)
 public class RestServiceSuperclass {
+
+  @GET
+  @Path("/ping")
+  public Response ping() {
+    return Response.ok().build();
+  }
 
 }
