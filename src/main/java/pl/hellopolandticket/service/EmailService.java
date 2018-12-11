2250e7e02032f00a4363aa4a8341a7a42fa15e1d
lmrozek <lmrozek@fream.pl>
2018-12-11 13:38:35 +0100

    temporary fix for hpl-575

--- src/main/java/pl/hellopolandticket/service/EmailService.java
+++ src/main/java/pl/hellopolandticket/service/EmailService.java
@@ -79,10 +79,10 @@ public class EmailService extends ServiceSuperclass {
       MimeMessage message = new MimeMessage(session);
       message
           .setFrom(new InternetAddress(System.getProperty(MAIL_USERNAME_PROPERTY), MAIL_PERSONAL));
-      var bcc = bookingMarkedAsBoughtEvent.getPartnersEmails();
-      bcc.add(MAIL_TICKET_COPY);
-      message.setRecipients(BCC, bcc.stream().collect(Collectors.joining(",")));
-      // message.setRecipients(BCC, new InternetAddress[] {new InternetAddress(MAIL_TICKET_COPY)});
+      // var bcc = bookingMarkedAsBoughtEvent.getPartnersEmails();
+      // bcc.add(MAIL_TICKET_COPY);
+      // message.setRecipients(BCC, bcc.stream().collect(Collectors.joining(",")));
+      message.setRecipients(BCC, new InternetAddress[] {new InternetAddress(MAIL_TICKET_COPY)});
       message.setRecipients(TO, new InternetAddress[] {
           new InternetAddress(bookingMarkedAsBoughtEvent.getCustomerEmail())});
       message.setSubject(emailTemplate.getSubject(), "UTF-8");
