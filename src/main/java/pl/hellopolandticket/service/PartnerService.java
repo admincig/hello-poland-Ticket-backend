8f2d76db03a255df5d16e316a3290f0685d14c6c
lmrozek <lmrozek@fream.pl>
2019-02-06 09:50:23 +0100

    hpl-653 part 1

--- src/main/java/pl/hellopolandticket/service/PartnerService.java
+++ src/main/java/pl/hellopolandticket/service/PartnerService.java
@@ -21,7 +21,6 @@ import pl.hellopolandticket.model.auth.User;
 import pl.hellopolandticket.model.partner.Partner;
 import pl.hellopolandticket.security.password.PasswordEncoder;
 import pl.hellopolandticket.service.exception.ExceptionFactory;
-import pl.hellopolandticket.service.exception.badrequest.BadRequestException;
 
 @Stateless
 @LocalBean
@@ -38,10 +37,10 @@ public class PartnerService extends ServiceSuperclass {
   private ExceptionFactory exceptionFactory;
 
   public PartnerDTO save(PartnerDTO partner) {
-    var usersDTOs = partner.users;
-    if (usersDTOs == null || usersDTOs.isEmpty() || !isAtLeastOneUsher(usersDTOs)) {
-      throw new BadRequestException("Wymagany jest co najmniej jeden uzytkownik z rolą biletera.");
-    }
+    // var usersDTOs = partner.users;
+    // if (usersDTOs == null || usersDTOs.isEmpty() || !isAtLeastOneUsher(usersDTOs)) {
+    // throw new BadRequestException("Wymagany jest co najmniej jeden uzytkownik z rolą biletera.");
+    // }
 
     Partner partnerToPersist = Partner.builder().name(partner.name).email(partner.email).build();
     partnerDao.persist(partnerToPersist);
