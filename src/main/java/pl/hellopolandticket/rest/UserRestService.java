3f9700ceb21d8381971c0adcd6cdd0749c41f011
lmrozek <lmrozek@fream.pl>
2019-02-07 12:42:59 +0100

    hpl-622

--- src/main/java/pl/hellopolandticket/rest/UserRestService.java
+++ src/main/java/pl/hellopolandticket/rest/UserRestService.java
@@ -1,12 +1,15 @@
 package pl.hellopolandticket.rest;
 
+import static pl.hellopolandticket.model.auth.Role.ROLE_EXTERNAL_USER;
 import static pl.hellopolandticket.model.auth.Role.ROLE_USER;
 import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import pl.hellopoland.dto.UserAuthDTO;
 import pl.hellopolandticket.security.Authenticated;
 import pl.hellopolandticket.security.CurrentUser;
 import pl.hellopolandticket.service.UserService;
@@ -28,4 +31,13 @@ public class UserRestService extends RestServiceSuperclass {
   public Response userInfo() {
     return Response.ok(userService.findByEmail(currentUser.getPrincipal())).build();
   }
+
+  @PUT
+  @Path("/password")
+  @RolesAllowed({ROLE_EXTERNAL_USER})
+  public Response changePassword(UserAuthDTO user) {
+    userService.changePassword(user.password, currentUser);
+    return Response.ok().build();
+  }
+
 }
--- src/main/java/pl/hellopolandticket/service/UserService.java
+++ src/main/java/pl/hellopolandticket/service/UserService.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import pl.hellopoland.dto.UserDTO;
 import pl.hellopolandticket.dao.UserDao;
 import pl.hellopolandticket.model.auth.User;
+import pl.hellopolandticket.security.CurrentUser;
 import pl.hellopolandticket.service.exception.ExceptionFactory;
 
 @Stateless
@@ -31,5 +32,9 @@ public class UserService extends ServiceSuperclass {
 
   public User save(User user) {
     return userDao.persist(user);
+  }
+
+  public void changePassword(String password, CurrentUser currentUser) {
+    findUserByEmail(currentUser.getPrincipal()).setPassword(password);
   }
 }
