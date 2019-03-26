0f5ef4303b87f45fccc02a4b84dbec3034201ab0
lmrozek <lmrozek@fream.pl>
2019-03-26 13:59:37 +0100

    hpl-726

--- src/main/java/pl/hellopolandticket/service/PartnerService.java
+++ src/main/java/pl/hellopolandticket/service/PartnerService.java
@@ -2,6 +2,7 @@ package pl.hellopolandticket.service;
 
 import static pl.hellopolandticket.model.auth.Role.ROLE_ADMIN;
 import static pl.hellopolandticket.model.auth.Role.ROLE_EXTERNAL_USER;
+import static pl.hellopolandticket.model.auth.Role.ROLE_SALESMAN;
 import static pl.hellopolandticket.model.auth.Role.ROLE_USHER;
 import static pl.hellopolandticket.model.auth.User.createHiddenUser;
 import static pl.hellopolandticket.model.auth.User.createUsher;
@@ -39,7 +40,7 @@ public class PartnerService extends ServiceSuperclass {
   @Inject
   private ExceptionFactory exceptionFactory;
 
-  @RolesAllowed({ROLE_ADMIN})
+  @RolesAllowed({ROLE_ADMIN, ROLE_SALESMAN})
   public PartnerDTO save(PartnerDTO partner) {
     Partner partnerToPersist = Partner.builder().name(partner.name).email(partner.email).build();
     partnerDao.persist(partnerToPersist);
--- src/main/java/pl/hellopolandticket/service/UserService.java
+++ src/main/java/pl/hellopolandticket/service/UserService.java
@@ -2,6 +2,7 @@ package pl.hellopolandticket.service;
 
 import static pl.hellopolandticket.model.auth.Role.ROLE_ADMIN;
 import static pl.hellopolandticket.model.auth.Role.ROLE_EXTERNAL_USER;
+import static pl.hellopolandticket.model.auth.Role.ROLE_SALESMAN;
 import static pl.hellopolandticket.model.auth.Role.ROLE_USHER;
 import static pl.hellopolandticket.service.util.ModelObjectsToDTOConverter.ofUser;
 import java.util.List;
@@ -68,7 +69,7 @@ public class UserService extends ServiceSuperclass {
         userDao.findByEmail(email).orElseThrow(() -> exceptionFactory.resourceNotFoundException()));
   }
 
-  @RolesAllowed({ROLE_ADMIN})
+  @RolesAllowed({ROLE_ADMIN, ROLE_SALESMAN})
   public User save(User user) {
     return userDao.persist(user);
   }
