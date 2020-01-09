4698960d583126c299af19ddaef062cce2216345
wiktor <wwolakksiazek@fream.pl>
2020-01-09 14:53:04 +0100

    creating one comparer

--- src/main/java/pl/hellopolandticket/service/TicketPoolDefinitionService.java
+++ src/main/java/pl/hellopolandticket/service/TicketPoolDefinitionService.java
@@ -192,17 +192,20 @@ public class TicketPoolDefinitionService extends ServiceSuperclass {
   }
 
   private void updateAtnas(TicketPoolDefinition tpd, TicketPoolDefinitionDTO dto) {
-    updateAtnasTicketNumber(tpd, dto);
-    updateAtnasDiscount(tpd, dto);
+    TicketPoolDefinitionAtnasComparer comparer = new TicketPoolDefinitionAtnasComparer(tpd);
+    updateAtnasTicketNumber(comparer, dto);
+    updateAtnasDiscount(comparer, dto);
   }
 
-  private void updateAtnasDiscount(TicketPoolDefinition tpd, TicketPoolDefinitionDTO dto) {
-    var diffs = new TicketPoolDefinitionAtnasComparer(tpd).getDiscountDifferences(dto);
+  private void updateAtnasDiscount(TicketPoolDefinitionAtnasComparer comparer,
+      TicketPoolDefinitionDTO dto) {
+    var diffs = comparer.getDiscountDifferences(dto);
     new TicketPoolDefinitionAtnasDiffApplier(atnaService).applyDiscountUpdate(diffs);
   }
 
-  private void updateAtnasTicketNumber(TicketPoolDefinition tpd, TicketPoolDefinitionDTO dto) {
-    var diffs = new TicketPoolDefinitionAtnasComparer(tpd).getTicketNumberDifferences(dto);
+  private void updateAtnasTicketNumber(TicketPoolDefinitionAtnasComparer comparer,
+      TicketPoolDefinitionDTO dto) {
+    var diffs = comparer.getTicketNumberDifferences(dto);
     new TicketPoolDefinitionAtnasDiffApplier(atnaService).apply(diffs);
 
     var toInform = Stream.concat(
