2f239c85f4434043a9290bcc233e100593983fcc
wiktor <wwolakksiazek@fream.pl>
2020-01-09 17:22:26 +0100

    added update Discount method in atnas diff applier

--- src/main/java/pl/hellopolandticket/service/TicketPoolDefinitionAtnasDiffApplier.java
+++ src/main/java/pl/hellopolandticket/service/TicketPoolDefinitionAtnasDiffApplier.java
@@ -50,39 +50,78 @@ public class TicketPoolDefinitionAtnasDiffApplier {
 
   public void applyDiscountUpdate(TicketPoolDefinitionAtnasDiscountComparerResult diffs) {
     for (Entry<AvailableTicketNumberAssociation, TicketDefinitionDTO> newDiscount : diffs.toAdd) {
+      for (AvailableTicketNumberAssociation child : newDiscount.getKey().getChildren()) {
+        updateDiscount(child, newDiscount.getValue());
+      }
       updateDiscount(newDiscount.getKey(), newDiscount.getValue());
     }
 
     for (Entry<AvailableTicketNumberAssociation, TicketDefinitionDTO> remove : diffs.toRemove) {
+      for (AvailableTicketNumberAssociation child : remove.getKey().getChildren()) {
+        child.setDiscount(null);
+      }
       remove.getKey().setDiscount(null); // zadziała?
     }
 
     for (Entry<AvailableTicketNumberAssociation, TicketDefinitionDTO> modify : diffs.toModify) {
-
+      for (AvailableTicketNumberAssociation child : modify.getKey().getChildren()) {
+        updateDiscount(child, modify.getValue());
+      }
+      updateDiscount(modify.getKey(), modify.getValue());
     }
   }
 
   private void updateDiscount(AvailableTicketNumberAssociation atna, TicketDefinitionDTO dto) {
     Discount d = new Discount();
 
-    d.setHplOwner(dto.discountIsHplOwner.booleanValue());
+    // isowner
+    boolean isHplOwner = dto.discountIsHplOwner.booleanValue();
+    d.setHplOwner(isHplOwner);
 
-    if (atna.getDiscount().isHplOwner()) {
-
+    // hplpart
+    // partnerpart
+    if (isHplOwner) {
       d.setHplPart(dto.discountHplPart.intValue());
       d.setPartnerPart(dto.discountPartnerPart.intValue());
-
-      // wyliczenia dla
-      // value, discountType
     } else {
       d.setHplPart(0);
       d.setPartnerPart(dto.discountPartnerPart.intValue());
-      // wyliczenia dla
-      // value, discountType
-
     }
 
+    // type
+    if (dto.discountType.toString().equalsIgnoreCase(Discount.Type.FLAT.toString())) {
+      d.setType(Discount.Type.FLAT);
+      d.setValue(dto.discountValue);
+    } else {
+      d.setType(Discount.Type.PERCENT);
+      d.setValue(dto.price * dto.discountValue);
+    }
+
+    // percent
+    if (Discount.Type.PERCENT.equals(d.getType())) {
+      d.setPercent(dto.discountValue);
+    }
+
+    // discountPrice
+    d.setDiscountPrice(dto.price - d.getValue());
+
+
+    validateDiscountValue(dto.price, d.getValue());
+    validateDiscountParts(d.getHplPart(), d.getPartnerPart(), d.getValue());
+
     atna.setDiscount(d);
+  }
+
+  private void validateDiscountValue(int price, int discountValue) {
+    if (price < discountValue) {
+      throw new ConflictingException("Discount cannot be bigger than ticket price");
+    }
+  }
+
+  private void validateDiscountParts(int hplPart, int partnerPart, int value) {
+    if (hplPart + partnerPart != value) {
+      throw new ConflictingException("Sum of discount parts is not equal to discount value");
+    }
   }
 
 }
