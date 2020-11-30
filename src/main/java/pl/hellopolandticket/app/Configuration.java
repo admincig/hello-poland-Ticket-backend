265f83e4b230c148dfb5b337b8ae823daae5130b
Dawid Olejnik <dawid@fream.pl>
2020-11-30 13:37:35 +0100

    different datasource definition. in web.xml

--- src/main/java/pl/hellopolandticket/app/Configuration.java
+++ src/main/java/pl/hellopolandticket/app/Configuration.java
@@ -12,14 +12,14 @@ import pl.hellopolandticket.security.password.PasswordEncoder;
 @Startup
 @Singleton
 // @Interceptors(value = LoggingHandler.class)
-@DataSourceDefinition(
-    name = "java:global/jdbc/hellopolandticketDS",
-    className = "org.postgresql.xa.PGXADataSource",
-    serverName = "database",
-    portNumber = 5432,
-    databaseName = "helloticket",
-    user = "helloticket",
-    password = "helloticket")
+//@DataSourceDefinition(
+//    name = "java:global/jdbc/hellopolandticketDS",
+//    className = "org.postgresql.xa.PGXADataSource",
+//    serverName = "database",
+//    portNumber = 5432,
+//    databaseName = "helloticket",
+//    user = "helloticket",
+//    password = "helloticket")
 public class Configuration {
 
   @Produces
