fc39e627d1474aba392685954324bc016175b05f
Dawid Olejnik <dawid@fream.pl>
2021-01-12 13:22:43 +0100

    no more compose, big change in CI variables

--- create-database.sql
+++
@@ -1,2 +0,0 @@
-create user helloticket with password 'helloticket';
-create database helloticket owner helloticket ENCODING = 'UTF8' TABLESPACE = pg_default LC_COLLATE = 'en_US.UTF-8' CONNECTION LIMIT = -1 TEMPLATE template0;
